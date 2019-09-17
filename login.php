8e2dca47eca27b4d217b38f430d39b16e2c3b0d8
samody2006 <samody2006@yahoo.com>
2019-09-17 06:12:50 +0100

    added Php

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+				if(isset($_POST['login'])){
+					include 'config.php';
+					
+					$email = $_POST['email'];
+					$psw = $_POST['psw'];
+					
+					$query = "SELECT * FROM admin WHERE email = '$email' AND psw = '$psw'";
+					$rs = $conn->query($query);
+					$num = $rs->num_rows;
+					$rows = $rs->fetch_assoc();
+					if($num > 0){
+						session_start();
+						$_SESSION['email'] = $rows['email'];
+						$_SESSION['psw'] = $rows['psw'];
+						echo "<script type = \"text/javascript\">
+									alert(\"Login Successful.................\");
+									window.location = (\"index.html\")
+									</script>";
+					} else{
+						echo "<script type = \"text/javascript\">
+									alert(\"Login Failed. Try Again................\");
+									window.location = (\"index.html\")
+									</script>";
+					}
+				}
+			?>
